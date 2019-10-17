953d5de425e57b60e6e799808561e2f273a66615
Eduardo Revilla Vaquero <edu.revilla.vaquero@gmail.com>
2019-10-17 19:53:07 +0200

    cambios 2019/10/17

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  num: number;
+  mayorMenor = '...';
+  numSecret: number = this.numAleatorio(0,100);
+ 
   constructor() {}
 
+  numAleatorio(a, b) {
+    return Math.round(Math.random() * (b - a) + parseInt(a, 10));
+  }
+
+  compruebaNumero(){
+    if(this.num)
+    {
+      if(this.numSecret < this.num)
+      {
+        this.mayorMenor = 'menor que';
+      }
+      else if(this.numSecret > this.num)
+      {
+        this.mayorMenor = 'mayor que';
+      }
+      else{
+        this.mayorMenor = '';
+      }
+    }
+  }
+
+  reinicia(){
+    // reiniciamos las variables
+    this.num = null;
+    this.mayorMenor = '...';
+    this.numSecret = this.numAleatorio(0,100);
+  }
+  
 }
